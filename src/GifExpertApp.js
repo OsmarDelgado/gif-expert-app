c42657330360116fef099a6745ed02e7a1182176
OsmarDelgado <osmar.mas@correo.uady.mx>
2021-10-15 12:12:57 -0500

    Basic finished component AddCategory and for send through components

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
+import AddCategory from "./components/AddCategory";
 
 const GifExpertApp = () => {
-    // const categories = [ 'One Punch', 'Samurai X', 'Dragon Ball' ]
     const [ categories, setCategories ] = useState( [ 'One Punch', 'Samurai X', 'Dragon Ball' ] )
 
-    const handleAdd = () => {
-        // setCategories( [ ...categories, 'HunterXHunter' ] )
-        setCategories( cats => [ ...cats, 'HunterXHunter' ] )
-    }
+    // const handleAdd = () => {
+    //     // setCategories( [ ...categories, 'HunterXHunter' ] )
+    //     setCategories( cats => [ ...cats, 'HunterXHunter' ] )
+    // }
 
     return (
         <>
             <h2>GifExpertApp</h2>
-            <hr />
 
-            <button onClick={ handleAdd }>Add</button>
+            <AddCategory setCategories={ setCategories } />
+
+            <hr />
 
             <ol>
                 {
